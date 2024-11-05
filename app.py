f17189ad0ef34574d9c58bad0e253de139ae0b6a
miki77 <mihail.konstantinov.05@gmail.com>
2024-11-05 14:20:08 +0300

    Process update

---
+++ app.py
@@ -0,0 +1,45 @@
+import ast
+
+#Словарь для определения структуры данных
+Structure_dct = {}
+
+def processing_str_code(code : list[str]):
+    '''Функция для обработки строк кода'''
+    new_code = []
+    for i in range(len(code)):
+        if not(code[i] == '\n'):
+            code[i] = code[i].replace('    ', '\t')
+            new_code.append(code[i])
+
+    return new_code
+
+
+def add_string_child(text_code : list[str]) -> list[str]:
+    '''Функция переработки строк кода из списка
+        Сложение строк, которые вложенны в циклы, условия, функции'''
+    new_code = []
+    temp_line = text_code[0]
+
+    for i in range(1, len(text_code)):
+        if not('    ' in text_code[i]):
+            new_code.append(temp_line)
+            temp_line = text_code[i]
+        else:
+            temp_line += text_code[i]
+        
+    if not(temp_line in new_code):
+        new_code.append(temp_line)
+
+    return processing_str_code(new_code)
+
+
+
+# Считывание строк кода с файла
+with open('file.py', 'r+') as file:
+    code_file = file.readlines()
+
+# Преобразование строк кодав блоки
+new_code = add_string_child(code_file)
+print(new_code)
+
+
---
+++ file.py
@@ -0,0 +1,11 @@
+x = 10
+n = 100
+
+for i in range(n):
+    print('!!!')
+
+print('Hello!')
+
+if n > 10:
+    print('Hello')
+    print('qq')
